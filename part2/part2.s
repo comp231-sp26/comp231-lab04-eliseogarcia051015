a66c19f33dcb6c93da6dc315e455fb5538708a29
Jorge Garcia <jorgeeliseo10@icloud.com>
2026-04-07 20:43:31 -0500

    Functional part2

--- part2/part2.s
+++ part2/part2.s
@@ -4,24 +4,36 @@
 _start:                             
           ldr		sp, =stack
           mov		r5, #0	//counter
-          mov 		r6, #0	//flag
+          mov 		r6, #0	//flag	
+          
+          ldr     	r1, =0xFF20005C     //clear edge-capture at startup
+          mov     	r0, #0xF
+          str     	r0, [r1]
           b			display
           
 loop:
 		  cmp		r6, #1	
           beq		do_delay
           
-Polling:
-		ldr			r1, =0XFF20005C	//Edgecapture register
-        ldr 		r0, [r1]
-        and			r0, r0, #0xF		//KEY0-KEY3
-        cmp			r0, #0
-        beq			Polling			//nothing pressed, keep looping
-        
-        mov			r0, #0
-        str			r0, [r1]
-        mov			r6, #1
-        b			loop
+polling:
+        ldr     r1, =0xFF200050     
+        ldr     r0, [r1]
+        and     r0, r0, #0xF
+        cmp     r0, #0              //0 means pressed
+        beq     polling             // pressed, keep looping
+
+release:
+        ldr     r1, =0xFF200050
+        ldr     r0, [r1]
+        and     r0, r0, #0xF
+        cmp     r0, #0
+        bne     release             //not pressed, keep waiting
+		
+        eor     r6, r6, #1          //flip: 0->1, 1->0
+        ldr     r1, =0xFF20005C     //clear edgecapture
+        mov     r0, #0xF
+        str     r0, [r1]
+        b       loop
         
 do_delay:
 		ldr			r1, =0XFF20005C	//Edgecapture register
@@ -32,7 +44,7 @@ do_delay:
         
         mov			r0, #0
         str			r0, [r1]
-        mov			r6, #0
+        eor     r6, r6, #1 
         b			loop
         
 run_delay:
@@ -61,7 +73,7 @@ display:
         bl			seg7_code	//r0
         
         //HEX0, HEX1
-        lsl			r3, r3, #7
+        lsl			r3, r3, #8
         orr			r0, r0, r3
         ldr			r8, =0xFF200020
         str			r0, [r8]
@@ -83,15 +95,15 @@ div_done:
         mov			r0, r1		//r0 = quotient
         mov			r1, r2		//r1 = remainder
         bx			lr
-
-.data
-bit_codes:  .byte   0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110
-            .byte   0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111
-            .skip   2      // pad with 2 bytes to maintain word alignment
-
+        
 seg7_code:  ldr     r1, =bit_codes  
             ldrb    r0, [r1, r0]    
-            bx      lr              
-  
+            bx      lr     
+
+bit_codes:  .byte   0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110
+            .byte   0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111
+            .skip   2      // pad with 2 bytes to maintain word alignment         
+ 			
+            .space	128 
 stack:         
 .end
